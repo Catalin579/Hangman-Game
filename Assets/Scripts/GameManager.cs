809815d49a020eb865781d250238be7aed0b388f
Catalin Maeschi <catalin.maeschi23@gmail.com>
2022-12-23 21:36:00 +0200

    Picking a Word
    -Picking the category
    -Picking a word from list or category
    -Split the word into single letters
    -Create the Visual

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,7 +9,11 @@ public class GameManager : MonoBehaviour
     public string[] unsolvedWord;
 
     public GameObject letterPrefab;
+    List<TMP_Text> letterHolderList = new List<TMP_Text>();
     public Transform letterHolder;
+
+    public Category[] categories;
+
 
     void Start()
     {
@@ -21,7 +27,27 @@ public class GameManager : MonoBehaviour
 
     void Initialize()
     {
+        //PICK A CATEGORY FIRST
+        int cIndex = Random.Range(0, categories.Length);
+        int wIndex = Random.Range(0, categories[cIndex].wordList.Length);
 
+        //PICK A WORD FROM LIST OR CATEGORY
+        string pickedWord = categories[cIndex].wordList[wIndex];
+
+        //SPLIT THE WORD INTO SINGLE LETTERS
+        string[] splittedWord = pickedWord.Select(l => l.ToString()).ToArray();
+        unsolvedWord = new string[splittedWord.Length];
+        foreach (string letter in splittedWord)
+        {
+            solvedList.Add(letter);
+        }
+
+        //CREATE THE VISUAL
+        for (int i = 0; i < solvedList.Count; i++)
+        {
+            GameObject tempLetter = Instantiate(letterPrefab, letterHolder, false);
+            letterHolderList.Add(tempLetter.GetComponent<TMP_Text>());
+        }
     }
 
     void CheckLetter(string requestedLetter)
